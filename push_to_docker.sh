b2b958381eb31ab6d292eaa7e52769c55efd1dfd
Ubuntu <ubuntu@ip-10-0-0-226.ec2.internal>
2016-11-13 18:31:28 +0000

    typo in container name from docker-compose

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,3 +1,3 @@
 docker login -e $DOCKER_EMAIL -p $DOCKER_PASSWORD -u $DOCKER_USER
-docker tag zenregistrar_web stevenzeiler/zen-registrar:latest
+docker tag zenregistrar_webapp stevenzeiler/zen-registrar:latest
 docker push stevenzeiler/zen-registrar:latest
