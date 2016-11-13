1de568c76e6ed86d004a2b3b290e75fe01716c9b
Ubuntu <ubuntu@ip-10-0-0-226.ec2.internal>
2016-11-13 02:38:51 +0000

    Generate base RegistrationsController with actions: create, show, index

---
+++ api/controllers/RegistrationsController.js
@@ -0,0 +1,41 @@
+/**
+ * RegistrationsController
+ *
+ * @description :: Server-side logic for managing Registrations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `RegistrationsController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `RegistrationsController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `RegistrationsController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  }
+};
+
